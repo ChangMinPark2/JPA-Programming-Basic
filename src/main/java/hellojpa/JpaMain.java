f7050b7c9c141df75e98e5ad9e69823ddb0f2db5
ChangMinPark2 <98qkrckdals@naver.com>
2024-03-18 11:34:11 +0900

    feat(JPA): 1차캐시 이해하기

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,8 +20,14 @@ public class JpaMain {
             member.setName("HelloA");
             // 여기까지 비영속 상태
 
-            em.persist(member);
+            em.persist(member); //1차캐시 저장
             // 영속 상태
+
+            Member findMember = em.find(Member.class, 100L);
+
+            System.out.println("--------------" + findMember.getId());
+            System.out.println("--------------" + findMember.getName());
+            // DB에서 select문 가져오는게 아닌, 1차캐시에서 가져옴.
 
             tx.commit();
             //DB에 쿼리가 나가는 시점
